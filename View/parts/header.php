20a3b29a81846e71379335ffe5c65a4140f69b4e
Rick nelber <ricardo.nelber.70@gmail.com>
2025-06-11 19:52:14 -0300

    Atualização de carrinho e view

---
+++ View/parts/header.php
@@ -0,0 +1,33 @@
+    <nav class="navbar">
+        <div class="navbar-left">
+            <div class="logo-container">
+                <a href="#" class="logo-link">
+                    <img src="../icons/Emporio_Jaco_2.png" alt="Logo" class="logo-nav">
+                </a>
+            </div>
+        </div>
+        <div class="navbar-right">
+            <a href="index.html" class="nav-link" id="nav-link">
+                <img src="../icons/casa.png" alt="Início" class="nav-icon"> 
+                <span>Início</span>
+            </a>
+            <a href="#servicos" class="nav-link" id="nav-link">
+                <img src="../icons/entregavel.png" alt="Produtos" class="nav-icon"> 
+                <span>Produtos</span>
+            </a>
+            <a href="#contato" class="nav-link" id="nav-link">
+                <img src="../icons/loc.png" alt="Endereço e Horário" class="nav-icon">
+                <span>Endereço e Horário</span>
+            </a>
+
+            <a href="" class="nav-link" id="nav-link">
+                <img src="../icons/entregavel.png" alt="Cadastrar" class="nav-icon">
+                <span>Cadastrar</span>
+            </a>
+
+            <a href="<?php ?>" class="nav-link" id="nav-link">
+                <img src="../icons/entregavel.png" alt="Entrar" class="nav-icon">
+                <span>Entrar</span>
+            </a>
+        </div>
+    </nav>
